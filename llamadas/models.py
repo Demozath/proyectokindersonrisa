e0fb8e61d37adfcadb81d7bc166e4023b7b33cac
Demozath <abeljsmz@gmail.com>
2023-11-02 00:01:01 -0300

    Se agrega a la bbdd los pacientes. Al template de registro de llamadas ahora muestra las llamadas realizadas.

--- llamadas/models.py
+++ llamadas/models.py
@@ -16,3 +16,14 @@ class Llamada(models.Model):
 
     def __str__(self):
         return f'{self.usuario.rut} - {self.tipo.nombre}'
+
+class Pacientes(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    rut = models.CharField(max_length=12, primary_key=True, unique=True)
+    numero_telefono = models.IntegerField(unique=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
--- llamadas/views.py
+++ llamadas/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from .models import Llamada
+from .models import Llamada, TipoLlamada
 from .forms import LlamadaForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
@@ -7,6 +7,11 @@ from django.contrib import messages
 class LlamadaListView(ListView):
     model = Llamada
     template_name = 'llamada_list.html'
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Llamada, TipoLlamada
+from .forms import LlamadaForm
 
 def registrar_llamada(request):
     if request.method == 'POST':
@@ -19,4 +24,23 @@ def registrar_llamada(request):
             return redirect('registrar_llamada')
     else:
         form = LlamadaForm()
-    return render(request, 'llamadas/registrar_llamada.html', {'form': form})
+
+    # Obtenemos todas las llamadas del usuario actual
+    llamadas = Llamada.objects.filter(usuario=request.user)
+
+    # Traemos todos los tipos de llamada
+    todos_los_tipos = TipoLlamada.objects.all()
+
+    # Creamos un diccionario con el conteo de llamadas y comentarios por tipo para el usuario
+    datos_por_tipo = {}
+    for tipo in todos_los_tipos:
+        conteo = llamadas.filter(tipo=tipo).count()
+        comentarios = [llamada.comentario for llamada in llamadas.filter(tipo=tipo)]
+        datos_por_tipo[tipo.nombre] = {
+            'conteo': conteo,
+            'comentarios': comentarios
+        }
+
+    return render(request, 'llamadas/registrar_llamada.html', {'form': form, 'datos_por_tipo': datos_por_tipo})
+
+
