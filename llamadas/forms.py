b57c638f8e474d34e1da1c644ac62873bc3dc20d
Demozath <abeljsmz@gmail.com>
2023-11-16 15:39:42 -0300

    Se agrega CSS de reigstrar_llamada

--- llamadas/forms.py
+++ llamadas/forms.py
@@ -4,7 +4,7 @@ from .models import Llamada, TipoLlamada
 class LlamadaForm(forms.ModelForm):
     class Meta:
         model = Llamada
-        fields = ['tipo', 'comentario']
+        fields = ['tipo']
 
 class FilterForm(forms.Form):
     desde = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
